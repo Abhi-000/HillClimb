b87600f6410b547fb5c9bc0b23e4dbb76bed1ec6
Abhishek-69 <60704699+Abhishek-69@users.noreply.github.com>
2020-12-22 16:57:47 +0530

    HillClim
    -First Push

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Rigidbody2D frontWheel;
+    public Rigidbody2D backWheel;
+    public Rigidbody2D Car;
+    public float carSpeed =50f;
+    public float carTorque= -100f;
+    float input;
+    // Start is called before the first frame update
+    void FixedUpdate()
+    {
+        frontWheel.AddTorque(input * carSpeed * Time.fixedDeltaTime);
+        backWheel.AddTorque(input * carSpeed * Time.fixedDeltaTime);
+        Car.AddTorque(input * carTorque * Time.fixedDeltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = -Input.GetAxis("Horizontal");
+    }
+}
---
+++ Assets/Scripts/TriggerDeath.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TriggerDeath : MonoBehaviour
+{
+    public HingeJoint2D head;
+    public WheelJoint2D[] tires;
+    public GameObject[] bodyParts = new GameObject[10];
+    public GameObject parentObj;
+    private bool isDead;
+    public void Start()
+    {
+        print(parentObj.name + ":" + parentObj.transform.localEulerAngles.z);
+        isDead = false;
+        Time.timeScale = 1f;
+        for (int i = 0;i<5;i++)
+        {
+            bodyParts[i] = parentObj.transform.Find("body" + i).gameObject;
+        }
+        tires = parentObj.GetComponents<WheelJoint2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ground")
+        {
+            StartCoroutine(LevelRestart());
+        }
+    }
+    public void Update()
+    {
+        //print(parentObj.name+":"+parentObj.transform.localEulerAngles.z);
+       if(parentObj.transform.localEulerAngles.z == 360)
+        {
+            print("Back Flip");
+        }
+       else if(parentObj.transform.localEulerAngles.z == -360)
+        {
+            print("front Flip");
+        }
+    }
+
+    IEnumerator LevelRestart()
+    {
+        isDead = true;
+        yield return new WaitForSeconds(0.25f);
+/*        for (int i = 0; i < 5; i++)
+        {
+            bodyParts[i].AddComponent<Rigidbody2D>();
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            bodyParts[i].GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+        }*/
+        head.enabled = false;
+        for (int i = 0; i < tires.Length; i++)
+        {
+            tires[i].enabled = false;
+        }
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene("SampleScene");
+
+    }
+}
